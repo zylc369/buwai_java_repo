26360c9ab877acdf5f4fa2af01d254d4f1eaff8d
不歪 <buwai.bw@qq.com>
2019-06-21 16:38:37 +0800

    提交

---
+++ source/buwai-jar-project/db/src/test/java/buwai/commons/db/jdbc/connect/MySQLConnectTest.java
@@ -0,0 +1,19 @@
+package buwai.commons.db.jdbc.connect;
+
+import org.testng.annotations.Test;
+
+/**
+ * MySQLConnect测试
+ *
+ * @author 不歪
+ * @version 创建时间：2019-06-21 15:08
+ */
+public class MySQLConnectTest {
+
+    @Test
+    public void test() throws Exception {
+        MySQLConnect connect = new MySQLConnect(null, null);
+
+    }
+
+}
